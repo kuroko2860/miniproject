ec30435a8258a598f08709bd2c54dbc740e6088f
kuroko2860 <developer2860@gmail.com>
2024-05-30 20:03:26 +0700

    remove nginx and load balance

--- nodejsserver/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/miniproject")
-  .then(() => console.log("db connected !"));
-
-module.exports = mongoose;
--- test/cannontest.js
+++ test/cannontest.js
@@ -1,15 +1,11 @@
 const autocannon = require("autocannon");
-const { PassThrough } = require("stream");
 const dumpRequestData = require("./dumpdata");
 
 function run(url) {
-  const buf = [];
-  const outputStream = new PassThrough();
-
   const instance = autocannon({
     url,
     method: "POST",
-    connections: 1000, // Number of concurrent connections
+    connections: 100, // Number of concurrent connections
     pipelining: 100, // Number of requests per connection
     duration: 10, // Duration of test in seconds
     headers: {
@@ -18,12 +14,7 @@ function run(url) {
     body: JSON.stringify(dumpRequestData),
   });
 
-  autocannon.track(instance, { outputStream });
-
-  outputStream.on("data", (data) => buf.push(data));
-  instance.on("done", () => {
-    process.stdout.write(Buffer.concat(buf));
-  });
+  autocannon.track(instance);
 }
 
-run("http://localhost:9000/objects");
+run("http://localhost:8080/objects");
